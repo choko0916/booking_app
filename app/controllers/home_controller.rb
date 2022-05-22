fc633bc3e73737805c62dd716770177ffb1cca8e
choko0916 <n.yuka0702@gmail.com>
2022-05-23 02:05:09 +0900

    ドロップダウンメニュー作成中、うまくいかない

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @user = current_user
-    @profile = Profile.find(@user.id)
+    if  user_signed_in? 
+      @user = current_user
+      @profile = Profile.find(@user.id)
+    end
   end
 end
